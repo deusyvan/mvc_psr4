9b60f211e26c41bc55ef03d9de0b1f0cab983ec6
Deusyvan Silva <contato@dfsweb.com.br>
2020-01-24 15:51:39 -0300

    Projeto MVC com PSR4 E BOOTSTRAP

---
+++ Views/template.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Site</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/template.css" />
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.4.1.min.js" ></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/script.js" ></script>
+    </head>
+    
+    <body>
+    	<div class="topo">
+    		<div class="topoint">
+    			<div class="topoleft">Site</div>
+    			<div class="toporight"> - <a href="">Sair</a></div>
+    			<div style="clear:both"></div>
+    		</div>
+    	</div>
+    	<div class="container">
+            <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+    	</div>
+    </body>
+</html>
---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+require 'environment.php';
+
+$config = array();
+if(ENVIRONMENT == 'development') {
+	define("BASE_URL", "http://localhost/mvc_psr4/");
+	$config['dbname'] = 'mvc_psr4';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'dfsweb';
+	$config['dbpass'] = '28033011';
+} else {
+	define("BASE_URL", "http://localhost/mvc_psr4/");
+	$config['dbname'] = 'mvc_psr4';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'dfsweb';
+	$config['dbpass'] = '28033011';
+}
+
+global $db;
+try {
+	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+} catch(PDOException $e) {
+	echo "ERRO: ".$e->getMessage();
+	exit;
+}
